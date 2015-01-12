3dec8886f7e04eded248249adab6b70b1aebbe34
peteben <git.peteben@neverbox.com>
2015-01-12 12:02:10 -0500

    Added xPL_tester, a simple application to test sending and receiving xPL Messages

---
+++ xPL_tester/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace xPL_tester {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+				}
+			base.Dispose(disposing);
+			}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.OnBut = new System.Windows.Forms.Button();
+			this.OffBut = new System.Windows.Forms.Button();
+			this.QuitBut = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(13, 33);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(488, 184);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(104, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Messages Received";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 236);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Device";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(59, 233);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(36, 20);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.Text = "C1";
+			// 
+			// OnBut
+			// 
+			this.OnBut.Location = new System.Drawing.Point(121, 231);
+			this.OnBut.Name = "OnBut";
+			this.OnBut.Size = new System.Drawing.Size(75, 23);
+			this.OnBut.TabIndex = 4;
+			this.OnBut.Text = "ON";
+			this.OnBut.UseVisualStyleBackColor = true;
+			this.OnBut.Click += new System.EventHandler(this.OnBut_Click);
+			// 
+			// OffBut
+			// 
+			this.OffBut.Location = new System.Drawing.Point(202, 231);
+			this.OffBut.Name = "OffBut";
+			this.OffBut.Size = new System.Drawing.Size(75, 23);
+			this.OffBut.TabIndex = 5;
+			this.OffBut.Text = "OFF";
+			this.OffBut.UseVisualStyleBackColor = true;
+			this.OffBut.Click += new System.EventHandler(this.OffBut_Click);
+			// 
+			// QuitBut
+			// 
+			this.QuitBut.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.QuitBut.Location = new System.Drawing.Point(426, 231);
+			this.QuitBut.Name = "QuitBut";
+			this.QuitBut.Size = new System.Drawing.Size(75, 23);
+			this.QuitBut.TabIndex = 6;
+			this.QuitBut.Text = "Quit";
+			this.QuitBut.UseVisualStyleBackColor = true;
+			this.QuitBut.Click += new System.EventHandler(this.QuitBut_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(513, 269);
+			this.Controls.Add(this.QuitBut);
+			this.Controls.Add(this.OffBut);
+			this.Controls.Add(this.OnBut);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "xPL Tester";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+			}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button OnBut;
+		private System.Windows.Forms.Button OffBut;
+		private System.Windows.Forms.Button QuitBut;
+		}
+	}
+
---
+++ xPL_tester/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Crappy little program to test the ESP8266 sending and receiving xPL messages.
+// Normally, proper xPL programs make use of a 'hub' server, which repeats xPL messages to all
+// registered applications, but for simplicity, this app listens to every packet sent to port 3865.
+// Crude parsing is then used to display command packets that might be coming from the ESP8266
+
+// Do not use this as a basis to write any sort of proper xPL-compatible service or device!
+// Instead, use the official xPL libraries that can be found at http://xplproject.co.uk
+
+namespace xPL_tester {
+	public partial class Form1 : Form {
+		const string VENDORID = "peteben";
+		const string DEVICEID = "xpltester";
+		UdpClient cl;
+		IPEndPoint ep;
+
+		delegate void StringDelegate(string data);
+
+		public void ReceiveCallback(IAsyncResult ar) {
+			Byte[] receiveBytes = cl.EndReceive(ar, ref ep);
+			string receiveString = Encoding.ASCII.GetString(receiveBytes);
+			string st, scmd, sdevice;
+
+			// Crude parser looks for x10 command packets comming from the ESP8266
+			if (receiveString.Contains("ESP8266") && receiveString.Contains("x10.basic")) {
+				int pos = receiveString.IndexOf("command=");
+
+				if (pos >= 0) {
+					st = receiveString.Substring(pos);
+					int k = st.IndexOf('\n');
+					if (k >= 8) {
+						scmd = st.Substring(8, k - 8);
+						pos = receiveString.IndexOf("device=");
+						if (pos >= 0) {
+							st = receiveString.Substring(pos);
+							k = st.IndexOf('\n');
+							if (k >= 7) {
+								sdevice = st.Substring(7, k - 7);
+								Invoke(new StringDelegate(AddText), new[] { sdevice + " " + scmd });
+								}
+							}
+						}
+
+
+					}
+				}
+			cl.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+			}
+
+		public Form1() {
+			InitializeComponent();
+			cl = new UdpClient(3865);
+			ep = new IPEndPoint(IPAddress.Any, 3865);
+			cl.EnableBroadcast = true;
+
+			cl.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+			}
+
+		void AddText(string s) {
+			textBox1.Text = s + "\r\n" + textBox1.Text;
+			textBox1.Refresh();
+			}
+
+		// Send a UDP xPL message to the ESP8266, to turn the LED (GPIO0) on.
+		private void OnBut_Click(object sender, EventArgs e) {
+			string onMsg = "xpl-cmnd\n{\nhop=1\nsource=peteben-xpltester.ESP\ntarget=*\n}\nx10.basic\n{\ncommand=on\ndevice=" + textBox2.Text +"\n}\n";
+			Byte[] sendBytes = Encoding.ASCII.GetBytes(onMsg);
+			try{
+				cl.Send(sendBytes, sendBytes.Length,new IPEndPoint(IPAddress.Broadcast, 3865));
+				}
+			catch ( Exception ex ){
+				}
+			}
+
+		// Send a UDP xPL message to the ESP8266, to turn the LED (GPIO0) off.
+		private void OffBut_Click(object sender, EventArgs e) {
+			string offMsg = "xpl-cmnd\n{\nhop=1\nsource=peteben-xpltester.ESP\ntarget=*\n}\nx10.basic\n{\ncommand=off\ndevice=" + textBox2.Text + "\n}\n";
+			Byte[] sendBytes = Encoding.ASCII.GetBytes(offMsg);
+			try {
+				cl.Send(sendBytes, sendBytes.Length, new IPEndPoint(IPAddress.Broadcast, 3865));
+				}
+			catch (Exception ex) {
+				}
+			}
+
+		private void QuitBut_Click(object sender, EventArgs e) {
+			Close();
+			}
+		}
+	}
